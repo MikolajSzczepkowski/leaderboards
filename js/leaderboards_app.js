9ca3f6343f2a06fb7686de208d2ce2e6e5edd5e5
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-21 11:02:12 +0100

    no table click color

--- js/leaderboards_app.js
+++ js/leaderboards_app.js
@@ -1,10 +1,10 @@
 $(function (){
 	var pageWidth = $(document).width();
 	$(document).on("click", ".table-leaderboard thead tr th", function(){
-		if (!$(this).hasClass("active")) {
-			$(".table-leaderboard thead tr th").removeClass("active");
+		if (!$(this).hasClass("current")) {
+			$(".table-leaderboard thead tr th").removeClass("current");
 			$("#changeOrder").remove();
-			$(this).addClass("active");
+			$(this).addClass("current");
 			if (pageWidth>=640) {
 				$(this).append("<img id='changeOrder' src='images/arrow-up.png'>");
 			}
@@ -17,12 +17,6 @@ $(function (){
 		if (!$(this).hasClass("current-game")) {
 			$("#gamesSlider ul li").removeClass("current-game");
 			$(this).addClass("current-game");
-		}
-	});
-	$(document).on("click", ".table-leaderboard tbody tr", function(){
-		if (!$(this).hasClass("selected")) {
-			$(".table-leaderboard tbody tr").removeClass("selected");
-			$(this).addClass("selected");
 		}
 	});
 	$(document).on("click", ".leaderboard-menu #firstMenu a", function(){
